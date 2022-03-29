2ee3e1462a4bed5eb9ab1c86d7e699f396350055
Liam Stevens <liamstevens111@users.noreply.github.com>
2022-03-29 17:16:46 +0700

    Added service to handle the creation of users and asssociated test to ensure case in-sensitive emails

---
+++ googlesearchdataviewer/apps/users/services.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from .models import BaseUser
+
+
+def user_create(*, email: str, is_active: bool = True, is_admin: bool = False, password: Optional[str] = None) -> BaseUser:
+    user = BaseUser.objects.create_user(
+        email=email, is_active=is_active, is_admin=is_admin, password=password)
+
+    return user
