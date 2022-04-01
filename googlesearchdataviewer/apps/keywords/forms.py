21e3710788cc8734a27ba3335e734b35f3bfd76d
Liam Stevens <liamstevens111@users.noreply.github.com>
2022-04-02 01:15:15 +0700

    Fixed CSV upload validation and refactored said funictionality as a utility function

--- googlesearchdataviewer/apps/keywords/forms.py
+++ googlesearchdataviewer/apps/keywords/forms.py
@@ -1,5 +1,4 @@
 from django import forms
 
-
 class UploadCsvKeywordsForm(forms.Form):
     file = forms.FileField(allow_empty_file=False, widget=forms.FileInput(attrs={'accept':'text/csv'}))
--- googlesearchdataviewer/apps/keywords/urls.py
+++ googlesearchdataviewer/apps/keywords/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.KeywordListView.as_view(), name='keyword-list'),
     path('<int:id>', views.KeywordDetailView.as_view(), name='keyword-detail'),
-    path('upload', views.keyword_file_upload, name='keyword-file-upload'),
+    path('upload', views.KeywordFileUploadView.as_view(), name='keyword-file-upload'),
 ]
